e6ccdc6e91c15b97dd8a1bbb6a96fd1d7b1d92ed
Cristian Lupascu <cristian.lupascu@jlg.ro>
2017-05-24 20:00:27 +0300

    Modif default state

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ module.exports = routesConfig;
 /** @ngInject */
 function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
-  $urlRouterProvider.otherwise('/login');
+  $urlRouterProvider.otherwise('/');
 
   $stateProvider
     .state('login', {
